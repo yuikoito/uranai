12ea0ef35af799423ffd6ca274f78ee3f7af5da9
yuikoito <yuiko.dev@gmail.com>
2022-02-10 00:01:05 +0900

    chore: custom calender

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,19 @@
-import DateFnsUtils from '@date-io/date-fns';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { default as AdapterDateFns } from '@mui/lab/AdapterDateFns';
 import DatePicker from '@mui/lab/DatePicker';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
-import { format } from 'date-fns';
 import jaLocale from 'date-fns/locale/ja';
 import { useState } from 'react';
 
 export default function ViewsDatePicker() {
   const [value, setValue] = useState<Date>();
 
-  class JaLocalizedUtils extends DateFnsUtils {
-    // ヘッダ部分のテキストを取得するメソッド
-    getDatePickerHeaderText(date: Date) {
-      return format(date, 'M月d日(E)', { locale: this.locale });
-    }
-  }
-
   return (
     <LocalizationProvider
       dateAdapter={AdapterDateFns}
+      dateFormats={{ monthAndYear: 'yyyy MM' }}
       locale={jaLocale}
-      utils={JaLocalizedUtils}
     >
       <Stack spacing={3}>
         <DatePicker
@@ -30,7 +21,7 @@ export default function ViewsDatePicker() {
           openTo="year"
           views={['year', 'month', 'day']}
           label="Year, month and date"
-          disableCloseOnSelect
+          disableCloseOnSelect={false}
           okText="閉じる"
           cancelText="キャンセル"
           value={value}
